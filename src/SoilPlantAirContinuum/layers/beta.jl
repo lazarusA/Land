c3f9e0b53dfe111d866b8140c42f669899438ca3
Reda El Ghawi <relghawi@bgc-jena.mpg.de>
2023-10-17 22:05:48 +0200

    changes to beta

--- src/SoilPlantAirContinuum/layers/beta.jl
+++ src/SoilPlantAirContinuum/layers/beta.jl
@@ -6,7 +6,7 @@ Compute the beta tuning factor for SPAC by taking the maximum, given
 - `beta` `BetaGLinearPsoil` type scheme
 
 """
-function spac_beta_max(spac::SPACMono{FT}, beta::BetaGLinearPsoil{FT}) where {FT<:AbstractFloat}
+function spac_beta_max(spac::SPACMono{FT}, beta::BetaGLinearPsoil{FT}, return_swc::Bool = false) where {FT<:AbstractFloat}
     _βm::FT = 0;
     swc_value::FT = zero(FT)
 
@@ -14,7 +14,12 @@ function spac_beta_max(spac::SPACMono{FT}, beta::BetaGLinearPsoil{FT}) where {FT
         _β = β_factor(spac.plant_hs.leaves[1], spac.plant_hs.roots[_i].sh, beta, FT(0), spac.plant_hs.roots[_i].p_ups, spac.swc[_i]);
         _βm = max(_β, _βm);
         swc_value = spac.swc[_i]
-    end;
+    end
 
-    return _βm,  swc_value 
+    if return_swc
+        return _βm, swc_value
+    else
+        return _βm
+    end
 end
+
